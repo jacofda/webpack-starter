c4a67e6a1e82039b51ee7c11c468ff0e28b1c83f
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2023-09-20 11:32:26 +0200

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "development",
+  entry: './index.ts',
+  mode: 'development',
   devServer: {
-    watchFiles: ["src/**/*"],
+    watchFiles: ['src/**/*'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, "src"),
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: "src/index.html", to: "index.html" },
-        { from: "src/assets", to: "assets"}
+        { from: '**/*.html', to: path.resolve(__dirname, 'dist') },
+        { from: 'assets', to: 'assets' },
       ],
     }),
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  context: path.resolve(__dirname, 'src'),
 };
